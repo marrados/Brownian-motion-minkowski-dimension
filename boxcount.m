303484ed85fd90c12e9e3d17917cedce3d805107
Paweł Bielicki <bielicki91@gmail.com>
2015-01-25 14:49:14 +0100

    dodanie funkcji liczacych wymiar minkowskiego -> jeszcze nie dziala

--- boxcount.m
+++ boxcount.m
@@ -7,13 +7,15 @@ function [ n ] = boxcount( X, box )
 %   number of box between vectors
 
 n = 0;
-[N, M] = size(X);
+N = size(X);
 
-for i = 1:1:N-1
-  p1 = X(i,:);
-  p2 = X(i+1,:);
-  b = mandist(p1, p2');
-  n = n + b/box;
+for i = 1 : N-1
+  p1 = X(i, :);
+  p2 = X(i+1, :);
+  d = mandist(p1', p2);
+  %d = sqrt(sum((p1 - p2) .^ 2));
+  b = max(1, d/box);
+  n = n + ceil(b);
 end
 
 end
