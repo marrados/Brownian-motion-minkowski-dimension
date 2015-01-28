2584fc90e0776f0438f3676c6a00cd829316022c
Paweł Bielicki <bielicki91@gmail.com>
2015-01-28 16:00:13 +0100

    usuniecie startowego wymiaru kostki na rzecz ilosci punkow pomiedzy

--- leastSquares.m
+++ leastSquares.m
@@ -9,10 +9,10 @@ Sxy = sum(x*y');
 
 a = (Sx*Sy - Sxy*n) / (Sx^2 - Sxx*n);
 b = (Sx*Sxy - Sxx*Sy) / (Sx*Sx - Sxx*n);
+clf
 hold on
 plot(x, y, 'b*');
 plot(x, a*x+b, 'r');
-%plotregression(y, x,'Regression');
 hold off
 
 end
--- minkowskiDim.m
+++ minkowskiDim.m
@@ -1,46 +1,44 @@
-function [ d, exitflag, maxIt ] = minkowskiDim( X, start, it, diff )
+function [ dim, exitflag, maxIt ] = minkowskiDim( X, it, p )
 %Count Minkowski dimension of m-dimension Brownian motion
 %Parameters:
 %   X - matrix with points of Brownian motion positions
-%   start - starting box length, 
-%       in each iteration box length is divided by 2
-%   it - number of iterations
+%   it - max number of iterations
+%   p - number of middle-points between points
+
 
 exitflag = 0;
 maxIt = 0;
 
 minB = min(X);
-%maxB = max(X);
-%diffB = abs(maxB - minB);
 X = X + ones(size(X))*diag(abs(minB));
+
+box = max(X(:)) - 1;
 
 Neps = zeros(1, it);
 eps = zeros(1, it);
-box = start;
 for i = 1 : it
-    Neps(i) = boxcount(X, box);
+    Neps(i) = boxcount(X, box, p);
     eps(i) = box;
-    box = box / diff;
+    box = box / 2;
+    
+    dim = leastSquares(-log10(eps(1:i)), log10(Neps(1:i)));
+    display(i);
+    display(dim);
     
     if i > 1 && Neps(i-1) == Neps(i)
         exitflag = -1;
         break;
     end
-    i
 end
 
 %take only non-zero results
 if exitflag == 0
-    i = i + 1
+    i = i + 1;
 end
 Neps = Neps(1:i-1);
 eps = eps(1:i-1);
 maxIt = i - 1;
 
-%a = log10(Neps) ./ -log10(eps)
-%d = leastSquares(-log10(eps), log10(Neps));
-
-d = leastSquares(log10(1./eps), log10(Neps));
+dim = leastSquares(-log10(eps), log10(Neps));
 
 end
-
